765715464ed4d1f48b9b0618c2ab8f9f3ce8a918
javachenxun <cxtiantian99@sina.com>
2022-05-14 21:25:06 +0800

    整理

---
+++ src/main/java/com/core/algorithm/kaoshi202205/package-info.java
@@ -0,0 +1,45 @@
+package com.core.algorithm.kaoshi202205;
+
+/**
+ * https://www.nowcoder.com/exam/oj?page=1&tab=%E7%AE%97%E6%B3%95%E7%AF%87&topicId=295
+ * 
+ * 1.Arrays: 排序，查找
+ *   java.util.Arrays#sort(int[], int, int)
+ *   java.util.Arrays#sort(long[])
+ *   java.util.Arrays#binarySearch(int[], int)
+ *
+ * 2.数据结构
+ *   数组
+ *
+ *   单链表：
+ *     虚头
+ *     提前记录当前节点（指针）
+ *   双链表：
+ *  
+ *   栈
+ *    Stack
+ *   队列
+ *   堆
+ *    PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(); //⼩顶堆，默认容量为11
+ *    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(11,new Comparator<Integer>(){
+ *            @Override
+ *            public int compare(Integer i1,Integer i2){
+ *                      return i2-i1;
+ *                  }
+ *            });//⼤顶堆
+ *
+ *   哈希
+ *
+ * 3.算法
+ *    查找：
+ *     链式查找法：链表或者数组一个个比较查找
+ *     二分查找法：
+*       一维数组，获取 最小（l），最大角标（r）去取中间角标m值，大于r=m-1，否则l=m+1
+ *      二维数组，取左下角值，大y移动（y--），小了x移动（x++），找不到不存在
+ *    排序：
+ *     归并排序，【BM20 数组中的逆序对】
+ *
+ *
+ *
+ */
+
--- src/main/java/com/core/algorithm/kaoshi202205/五哈希/package-info.java
+++
@@ -1,7 +0,0 @@
-package com.core.algorithm.kaoshi202205.五哈希;
-
-/**
- *
- * 1.主要利用map 做一些计算
- *
- */
