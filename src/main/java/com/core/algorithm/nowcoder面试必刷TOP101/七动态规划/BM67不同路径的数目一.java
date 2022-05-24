e98e8baa2c98b9d2efeca238d64aff85a4788158
javachenxun <cxtiantian99@sina.com>
2022-05-24 21:46:35 +0800

    斐波那契数列:BM67不同路径的数目

---
+++ src/main/java/com/core/algorithm/nowcoder面试必刷TOP101/七动态规划/BM67不同路径的数目一.java
@@ -0,0 +1,15 @@
+package com.core.algorithm.nowcoder面试必刷TOP101.七动态规划;
+
+/**
+ * Created by chenxun.
+ * Date: 2022/5/24 9:43 PM
+ * Description:
+ */
+public class BM67不同路径的数目一 {
+    public int uniquePaths (int m, int n) {
+        if(m==1||n==1){
+            return 1;
+        }
+        return uniquePaths(m-1,n)+uniquePaths(m,n-1);
+    }
+}
