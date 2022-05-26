2bdd29bddf71c31d8eeb61ac8ddffa34e2cf2cf9
javachenxun <cxtiantian99@sina.com>
2022-05-26 21:42:37 +0800

    BM63跳台阶

---
+++ src/main/java/com/core/algorithm/nowcoder面试必刷TOP101/七动态规划/BM63跳台阶.java
@@ -0,0 +1,19 @@
+package com.core.algorithm.nowcoder面试必刷TOP101.七动态规划;
+
+/**
+ * Created by chenxun.
+ * Date: 2022/5/26 9:37 PM
+ * Description:
+ */
+public class BM63跳台阶 {
+
+    public int jumpFloor(int target) {
+        int[] dp = new int[41];
+        dp[1]=1;
+        dp[2]=2;
+        for (int i = 3; i <41 ; i++) {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[target];
+    }
+}
