25fce0a58d8c2353f88032a68d01f40d5197689a
javachenxun <cxtiantian99@sina.com>
2022-06-06 17:49:06 +0800

    BM94接雨水问题

--- src/main/java/com/core/algorithm/nowcoder面试必刷TOP101/i九双指针/BM94接雨水问题.java
+++ src/main/java/com/core/algorithm/nowcoder面试必刷TOP101/i九双指针/BM94接雨水问题.java
@@ -8,33 +8,48 @@ package com.core.algorithm.nowcoder面试必刷TOP101.i九双指针;
 public class BM94接雨水问题 {
 
     public static long maxWater (int[] arr) {
-        //排除空数组
-        if(arr.length == 0)
+        if (arr == null || arr.length < 3) {
             return 0;
-        long res = 0;
-        //左右双指针
-        int left = 0;
-        int right = arr.length - 1;
-        //中间区域的边界高度
-        int maxL = 0;
-        int maxR = 0;
-        //直到左右指针相遇
-        while(left < right){
-            //每次维护往中间的最大边界
-            maxL = Math.max(maxL, arr[left]);
-            maxR = Math.max(maxR, arr[right]);
-            //较短的边界确定该格子的水量
-            if(maxR > maxL)
-                res += maxL - arr[left++];
-            else
-                res += maxR - arr[right--];
         }
-        return res;
+        /**
+         *
+         * 传统思想没求出来，写的巨复杂
+         *
+         * 参考：https://www.bilibili.com/video/BV1D34y1Y7xp?spm_id_from=333.337.search-card.all.click
+         *
+         * 1.每根柱子左右的最大值(提前动态规划写好)
+         * 2.每根柱子与左右最小值的值， 最小值大于当前柱子才能接雨水
+         * 3.累加求和
+         *
+         */
+
+        int[] arrLeft=new int[arr.length];
+        arrLeft[0]=arr[0];
+        for (int i = 1; i <arr.length ; i++) {
+            arrLeft[i]=Math.max(arrLeft[i-1],arr[i]);
+        }
+
+        int[] arrRight=new int[arr.length];
+        arrRight[arr.length-1]=arr[arr.length-1];
+        for (int i = arr.length-2; i >-1 ; i--) {
+            arrRight[i]=Math.max(arrRight[i+1],arr[i]);
+        }
+
+        int sum = 0 ;
+        for (int i = 1; i <arr.length-1 ; i++) {
+            int v = arr[i];
+            int min = Math.min(arrLeft[i - 1], arrRight[i + 1]);
+            if(min>v){
+                sum=sum+min-v;
+            }
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
-        int[] a = {3,1,1,1,1,1};
-        maxWater(a);
+        int[] a = {3,1,0,1,1,4};
+        long l = maxWater(a);
+        System.out.println(l);
     }
 
 }
