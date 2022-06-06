9046624bfa616993b2c9325d6aded48be19bdb2a
javachenxun <cxtiantian99@sina.com>
2022-06-06 19:20:43 +0800

    BM90最小覆盖子串

--- src/main/java/com/core/algorithm/nowcoder面试必刷TOP101/i九双指针/BM90最小覆盖子串.java
+++ src/main/java/com/core/algorithm/nowcoder面试必刷TOP101/i九双指针/BM90最小覆盖子串.java
@@ -1,5 +1,7 @@
 package com.core.algorithm.nowcoder面试必刷TOP101.i九双指针;
 
+import java.util.Arrays;
+
 /**
  * Created by chenxun.
  * Date: 2022/5/23 4:33 PM
@@ -7,63 +9,64 @@ package com.core.algorithm.nowcoder面试必刷TOP101.i九双指针;
  */
 public class BM90最小覆盖子串 {
 
+    static int[] pairs = null;
+    static int[] copyOf = null;
 
-    public String minWindow (String S, String T) {
+    public static String minWindow(String S, String T) {
         char[] charsS = S.toCharArray();
         char[] charsT = T.toCharArray();
 
-        int[] pairs = change(charsT);
+        pairs = new int[128];
+        copyOf = change(T.toCharArray());
 
-        int i = 0;
+        String res="";
         int start = 0;
         int end = 0;
-
-        int left = 0;
-        int right = 0;
-        boolean over = false;
-        while (i < charsS.length) {
-            if (!over) {
-                pairs[charsS[i]]--;
-                if (over(pairs)) {
-                    over = true;
-
-                    end = i;
-                    left = start;
-                    right = end;
+        while (start < charsS.length || end < charsS.length) {
+            boolean all = all(charsT);
+            int i = 0;
+            if (all && start < charsS.length) {
+                String value = String.valueOf(Arrays.copyOfRange(charsS, start, end));
+                if(res.equals("")){
+                    res=value;
                 }
-            } else {
-                if (over(pairs)) {
-                    char charsS1 = charsS[start];
-                    pairs[charsS1]--;
-                    start++;
-                } else {
-
-                }
-
+                res=value.length()>res.length()?res:value;
+                pairs[charsS[start]]--;
+                start++;
+                i++;
             }
-            i++;
+            if (!all && end < charsS.length) {
+                pairs[charsS[end]]++;
+                end++;
+                i++;
+            }
+            if (i == 0) {
+                break;
+            }
         }
-        return null;
+        return res;
     }
 
-    private boolean over(int[] pairs) {
-        for (int i = 0; i < 128; i++) {
-            if(pairs[i]>0){
+    private static boolean all(char[] chars) {
+        for (int i = 0; i < chars.length; i++) {
+            if (pairs[chars[i]] < copyOf[chars[i]]) {
                 return false;
             }
         }
         return true;
     }
 
-    private int[]  change(char[] charsT) {
+    private static int[] change(char[] chars) {
         int[] arr = new int[128];
-        for (int i = 0; i < 128; i++) {
-            arr[i]=-1;
-        }
-        for (char c:charsT){
+        for (char c : chars) {
             arr[c]++;
         }
         return arr;
     }
 
+
+    public static void main(String[] args) {
+        String s = minWindow("a", "a");
+        System.out.println(s);
+    }
 }
