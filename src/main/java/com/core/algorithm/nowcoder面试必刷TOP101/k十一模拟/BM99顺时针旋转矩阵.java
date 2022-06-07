db9f4d92bd6065f1236a24f6b244da9ac8f3393f
javachenxun <cxtiantian99@sina.com>
2022-06-07 15:41:59 +0800

    BM99顺时针旋转矩阵

--- src/main/java/com/core/algorithm/nowcoder面试必刷TOP101/k十一模拟/BM99顺时针旋转矩阵.java
+++ src/main/java/com/core/algorithm/nowcoder面试必刷TOP101/k十一模拟/BM99顺时针旋转矩阵.java
@@ -6,4 +6,14 @@ package com.core.algorithm.nowcoder面试必刷TOP101.k十一模拟;
  * Description:
  */
 public class BM99顺时针旋转矩阵 {
+    public int[][] rotateMatrix(int[][] mat, int n) {
+        //发现规律 行转变成列的关系
+        int[][] newArr=new int[n][n];
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j < n; j++) {
+                newArr[j][n-1-i]=mat[i][j];
+            }
+        }
+        return newArr;
+    }
 }
