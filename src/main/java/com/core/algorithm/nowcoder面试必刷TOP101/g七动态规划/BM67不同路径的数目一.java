49abbb0d7b298f5142f10676c96f343ed519ad07
javachenxun <cxtiantian99@sina.com>
2022-06-10 11:15:02 +0800

    二维数组的动态规划

--- src/main/java/com/core/algorithm/nowcoder面试必刷TOP101/g七动态规划/BM67不同路径的数目一.java
+++ src/main/java/com/core/algorithm/nowcoder面试必刷TOP101/g七动态规划/BM67不同路径的数目一.java
@@ -6,10 +6,39 @@ package com.core.algorithm.nowcoder面试必刷TOP101.g七动态规划;
  * Description:
  */
 public class BM67不同路径的数目一 {
-    public int uniquePaths (int m, int n) {
-        if(m==1||n==1){
-            return 1;
+    public static int uniquePaths (int m, int n) {
+        int[][] dp = new int[m][n];
+        //填充第一行
+        dp[0][0] = 1;
+        if (m > 1) {
+            for (int i = 1; i < m; i++) {
+                dp[i][0] = dp[i - 1][0];
+            }
         }
-        return uniquePaths(m-1,n)+uniquePaths(m,n-1);
+
+        //填充第一列
+        if (n > 1) {
+            for (int i = 1; i < n; i++) {
+                dp[0][i] = dp[0][i - 1];
+            }
+        }
+
+        for (int i = 1; i < m && m > 1; i++) {
+            for (int j = 1; j < n && n > 1; j++) {
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
+
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(2,7));
+    }
+
+//    public int uniquePaths2 (int m, int n) {
+//        if(m==1||n==1){
+//            return 1;
+//        }
+//        return uniquePaths(m-1,n)+uniquePaths(m,n-1);
+//    }
 }
---
+++ src/main/java/com/core/algorithm/nowcoder面试必刷TOP101/g七动态规划/BM68矩阵的最小路径和.java
@@ -0,0 +1,36 @@
+package com.core.algorithm.nowcoder面试必刷TOP101.g七动态规划;
+
+import java.util.Arrays;
+
+/**
+ * Created by chenxun.
+ * Date: 2022/6/9 11:41 PM
+ * Description:
+ */
+public class BM68矩阵的最小路径和 {
+    public static int minPathSum (int[][] matrix) {
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        //填充第一行
+        dp[0][0] = matrix[0][0];
+        for (int i = 1; i < matrix[0].length; i++) {
+            dp[0][i] = dp[0][i - 1] + matrix[0][i];
+        }
+        //填充第一列
+        for (int i = 1; i < matrix.length; i++) {
+            dp[i][0] = dp[i - 1][0] + matrix[i][0];
+        }
+        for (int i = 1; i < matrix[0].length; i++) {
+            for (int j = 1; j < matrix.length; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + matrix[i][j];
+            }
+        }
+        Arrays.stream(dp).forEach(item->System.out.println(Arrays.toString(item)));
+
+        return dp[matrix.length - 1][matrix[0].length - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{1,2,3},{1,2,3}};
+        System.out.println(minPathSum(matrix));
+    }
+}
